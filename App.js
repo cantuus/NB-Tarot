c4aac303ad53f690925bf8a86758ba8041e72fc4
Sophia Koeut <koeutsophia@gmail.com>
2020-06-15 00:10:16 -0700

    replaced initial text with custom title and image

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-export default function App() {
-  console.log('App executed');
-
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native!</Text>
+      <Text style={styles.text}>PADORU PADORU!</Text>
+      <Image source={{ uri: 'https://randomuser.me/api/portraits/men/1.jpg' }} style={styles.img} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  text: {
+    color: 'darkslateblue',
+    fontSize: 30
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  }
+})
+export default App;
