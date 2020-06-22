b4b3650ab72d04d1718fd5ca1d569f248b89f549
Sophia Koeut <koeutsophia@gmail.com>
2020-06-22 15:49:21 -0700

    implemented Platform, SafeView, Touchable, and Button modules

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert,
+  Platform,
+  StatusBar
+} from 'react-native';
 
 const App = () => {
+
+  const handlePress = () => console.log("Text pressed");
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>PADORU PADORU!!!!</Text>
-      <Image source={{ uri: 'https://randomuser.me/api/portraits/men/1.jpg' }} style={styles.img} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress} style={styles.text}>PADORU PADORU!!!!</Text>
+      <TouchableHighlight onPress={() => console.log("Image Tapped")}>
+        <Image
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"
+          }} />
+      </TouchableHighlight>
+
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() => Alert.prompt("my title", "my message", text => console.log(text))}
+      />
+    </SafeAreaView>
   );
 };
 
@@ -14,7 +44,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   text: {
     color: 'darkslateblue',
