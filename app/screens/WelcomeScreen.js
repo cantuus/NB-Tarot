a3e579ae3f3b6c490ca6e16835e4b0ee495a0032
Sophia Koeut <koeutsophia@gmail.com>
2020-06-25 18:54:15 -0700

    added pitch title

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -7,7 +7,10 @@ export default function WelcomeScreen() {
             style={styles.background}
             source={require('../assets/background.jpg')}>
 
-            <Image source={require("../assets/logo-red.png")} />
+            <View style={styles.logoContainer}>
+                <Image style={styles.baby} source={require("../assets/logo-red.png")} />
+                <Text>Sell What You Don't Need</Text>
+            </View>
             <View style={styles.loginButton}></View>
             <View style={styles.registerButton}></View>
 
@@ -19,16 +22,28 @@ export default function WelcomeScreen() {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        justifyContent: "flex-end"
+        justifyContent: "flex-end",
+        alignItems: "center",
     },
     loginButton: {
         width: '100%',
         height: 70,
         backgroundColor: "#fc5c65"
     },
+    baby: {
+        width: 100,
+        height: 100,
+
+    },
+    logoContainer: {
+        position: "absolute",
+        top: 70,
+        alignItems: "center"
+    },
     registerButton: {
         width: '100%',
         height: 70,
         backgroundColor: "#4ecdc4",
-    }
+    },
+
 })
